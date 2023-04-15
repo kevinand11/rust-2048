55100d194fcb7a204bc424305e6b9da81441355c
kevinand11 <kevinfizu@gmail.com>
2023-04-15 03:10:12 +0100

    add scoring mechanism

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,9 @@
-use rand::seq::SliceRandom;
+use rand::Rng;
 
 #[derive(Debug, Clone, Default)]
 pub struct GameData {
-    pub arr: [[u8; 4]; 4],
+    arr: [[u64; 4]; 4],
+    score: u64,
 }
 
 impl GameData {
@@ -13,8 +14,7 @@ impl GameData {
         &mut self,
         get_old_idx: F1,
         get_new_idx: F2,
-    ) {
-        let mut empty_spots: Vec<(usize, usize)> = Vec::new();
+    ) -> &Self {
         for row in 0..4 {
             for col in 1..4 {
                 for idx in 0..col {
@@ -30,6 +30,9 @@ impl GameData {
 
                     if into_val == 0 || into_val == val {
                         self.arr[new_idx.0][new_idx.1] += val;
+						if into_val == val {
+                        	self.score += self.arr[new_idx.0][new_idx.1];
+						}
                         self.arr[old_idx.0][old_idx.1] = 0;
                     } else {
                         break;
@@ -38,42 +41,56 @@ impl GameData {
             }
         }
 
-        for row in 0..4 {
-            for col in 0..4 {
-                if self.arr[row][col] == 0 {
-                    empty_spots.push((row, col));
-                }
-            }
-        }
-        let (row, col) = *empty_spots.choose(&mut rand::thread_rng()).unwrap();
-        self.arr[row][col] = 2;
+        self.add_random()
     }
 
-    pub fn up(&mut self) {
+    pub fn up(&mut self) -> &Self {
         self.move_arr(
             |row: usize, col: usize, idx: usize| (col - idx, row),
             |row: usize, col: usize, idx: usize| (col - idx - 1, row),
         )
     }
 
-    pub fn down(&mut self) {
+    pub fn down(&mut self) -> &Self {
         self.move_arr(
             |row: usize, col: usize, idx: usize| (3 - (col - idx), row),
             |row: usize, col: usize, idx: usize| (3 - (col - idx) + 1, row),
         )
     }
 
-    pub fn left(&mut self) {
+    pub fn left(&mut self) -> &Self {
         self.move_arr(
             |row: usize, col: usize, idx: usize| (row, col - idx),
             |row: usize, col: usize, idx: usize| (row, col - idx - 1),
         )
     }
 
-    pub fn right(&mut self) {
+    pub fn right(&mut self) -> &Self {
         self.move_arr(
             |row: usize, col: usize, idx: usize| (row, 3 - (col - idx)),
             |row: usize, col: usize, idx: usize| (row, 3 - (col - idx) + 1),
         )
+    }
+
+    fn add_random(&mut self) -> &Self {
+        let mut rng = rand::thread_rng();
+        let rnd = rng.gen_range(0..15);
+        let row: usize = rnd / 4;
+        let col = rnd % 4;
+
+        if self.arr[row][col] != 0 {
+            return self.add_random();
+        }
+
+        self.arr[row][col] = 2;
+
+        self
+    }
+
+    pub fn new() -> GameData {
+        let mut game = GameData::default();
+        game.add_random();
+        game.add_random();
+        game
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,22 @@ mod data;
 fn main() {
     println!("Hello, world!");
 
-    let mut data = GameData {
-        arr: [[0, 4, 0, 2], [8, 2, 0, 8], [0, 0, 0, 2], [2, 0, 2, 2]],
-    };
-
+    let mut data = GameData::new();
     println!("Left\n{:?}", data);
     data.left();
     println!("{:?}", data);
 
-    data = GameData {
-        arr: [[2, 0, 4, 0], [8, 0, 2, 8], [2, 0, 0, 0], [2, 2, 0, 2]],
-    };
-
+    data = GameData::new();
     println!("\n\nRight\n{:?}", data);
     data.right();
     println!("{:?}", data);
 
-    data = GameData {
-        arr: [[2, 8, 2, 2], [0, 0, 0, 2], [4, 2, 0, 0], [0, 8, 0, 2]],
-    };
-
+    data = GameData::new();
     println!("\n\nUp\n{:?}", data);
     data.up();
     println!("{:?}", data);
 
-    data = GameData {
-        arr: [[2, 8, 2, 2], [0, 0, 0, 2], [4, 2, 0, 0], [0, 8, 0, 2]],
-    };
-
+    data = GameData::new();
     println!("\n\nDown\n{:?}", data);
     data.down();
     println!("{:?}", data);
